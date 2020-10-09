d1e39a14016815b2ee350139965de4eed3e6abfe
NaotoToriizuka <toriizuka.com@gmail.co>
2020-10-09 21:23:09 +0900

    ApplicationController記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
+require 'line/bot'
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :excetion
+  before_action :validate_signature, except: [:new, :create]
+
+  def validate_signature
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+  end
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_select = "your channel secret"
+      config.channel_token = "your channel token"
+    }
+  end
 end
---
+++ app/controllers/linebot_controller.rb
@@ -0,0 +1,3 @@
+class LinebotController < ApplicationController
+  
+end
